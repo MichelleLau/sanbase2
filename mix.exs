941e28f31d18bdd362c2e05635b2739b3fb9f863
Ivan Ivanov <ivan.ivanoff.95@gmail.com>
2018-07-05 17:30:46 +0300

    Update httpoison dependency and add ecto clickhouse

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Sanbase.Mixfile do
       {:absinthe_plug, "~> 1.4.0"},
       {:faktory_worker_ex, git: "https://github.com/santiment/faktory_worker_ex"},
       {:temp, "~> 0.4"},
-      {:httpoison, "~> 0.13"},
+      {:httpoison, "~> 1.2", override: true},
       {:floki, "~> 0.20"},
       {:sentry, "~> 6.0.4"},
       {:extwitter, "~> 0.9.0"},
@@ -91,7 +91,8 @@ defmodule Sanbase.Mixfile do
       {:plug_attack, "~> 0.3.1"},
       {:earmark, "~> 1.2"},
       {:ecto_enum, "~> 1.1"},
-      {:ex_machina, "~> 2.2", only: :test}
+      {:ex_machina, "~> 2.2", only: :test},
+      {:clickhouse_ecto, "~> 0.2"}
     ]
   end
 
