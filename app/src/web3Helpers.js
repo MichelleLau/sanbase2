2adba0849ad9f24cbd20a2a2e58f3e59ad5bc675
DmitriVanGuard <dimavanguard@gmail.com>
2018-07-21 21:51:27 +0300

    web3Helpers back

--- app/src/web3Helpers.js
+++ app/src/web3Helpers.js
@@ -10,37 +10,37 @@ export const setupWeb3 = cbk => {
   if (typeof window.web3 === 'undefined') {
     return
   }
-  // const localWeb3 = new Web3(web3.currentProvider)
-  // // Why the interval method here? ==> https://github.com/MetaMask/faq/blob/master/DEVELOPERS.md
-  // setInterval(() => {
-  //   const selectedAccount = localWeb3.eth.accounts[0]
-  //   if (prevSelectedAccount !== selectedAccount) {
-  //     prevSelectedAccount = selectedAccount
-  //     cbk(false, selectedAccount || null)
-  //   }
-  // }, 100)
+  const localWeb3 = new Web3(web3.currentProvider)
+  // Why the interval method here? ==> https://github.com/MetaMask/faq/blob/master/DEVELOPERS.md
+  setInterval(() => {
+    const selectedAccount = localWeb3.eth.accounts[0]
+    if (prevSelectedAccount !== selectedAccount) {
+      prevSelectedAccount = selectedAccount
+      cbk(false, selectedAccount || null)
+    }
+  }, 100)
 }
 
 export const signMessage = account => {
   const message = `Login in Santiment with address ${account}`
-  // const localWeb3 = new Web3(web3.currentProvider)
-  // const messageHash = localWeb3.sha3(
-  //   '\x19Ethereum Signed Message:\n' + message.length + message
-  // )
-  // return new Promise((resolve, reject) => {
-  //   localWeb3.personal.sign(
-  //     localWeb3.fromUtf8(message),
-  //     account,
-  //     (error, signature) => {
-  //       if (!error) {
-  //         resolve({
-  //           messageHash,
-  //           signature
-  //         })
-  //       } else {
-  //         reject(error)
-  //       }
-  //     }
-  //   )
-  // })
+  const localWeb3 = new Web3(web3.currentProvider)
+  const messageHash = localWeb3.sha3(
+    '\x19Ethereum Signed Message:\n' + message.length + message
+  )
+  return new Promise((resolve, reject) => {
+    localWeb3.personal.sign(
+      localWeb3.fromUtf8(message),
+      account,
+      (error, signature) => {
+        if (!error) {
+          resolve({
+            messageHash,
+            signature
+          })
+        } else {
+          reject(error)
+        }
+      }
+    )
+  })
 }
